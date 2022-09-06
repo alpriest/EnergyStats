763f9cf14b3704ac671226646c5c82c26bd641a8
Al Priest <alpriest@gmail.com>
2022-09-06 05:59:32 +0100

    Initial Commit

---
+++ PV Stats/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PV Stats
+//
+//  Created by Alistair Priest on 06/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PV Stats/PV_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PV_StatsApp.swift
+//  PV Stats
+//
+//  Created by Alistair Priest on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PV_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
