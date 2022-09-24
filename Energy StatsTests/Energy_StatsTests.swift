399b364d5d471b0fa7254ec8372ba0fc3d761643
Al Priest <alpriest@gmail.com>
2022-09-24 21:12:59 +0100

    Update tests

--- Energy StatsTests/Energy_StatsTests.swift
+++ Energy StatsTests/Energy_StatsTests.swift
@@ -5,23 +5,21 @@
 //  Created by Alistair Priest on 10/09/2022.
 //
 
-import XCTest
 @testable import Energy_Stats
+import XCTest
 
 final class BatteryCalculatorTests: XCTestCase {
-
     func test_CalculatesRemainingTimeUntilFull() {
-        let sut = BatteryCalculator(capacitykW: 8.0)
-        let result = sut.batteryRemaining(batteryChargePowerkWH: 1.0, batteryStartOfCharge: 0.50)
+        let sut = BatteryCapacityCalculator(capacitykW: 8000, minimumSOC: 0.2)
+        let result = sut.batteryRemaining(batteryChargePowerkWH: 1.0, batteryStateOfCharge: 0.50)
 
         XCTAssertEqual(result, "Full in 4 hours")
     }
 
     func test_CalculatesRemainingTimeUntilEmpty() {
-        let sut = BatteryCalculator(capacitykW: 8.0)
-        let result = sut.batteryRemaining(batteryChargePowerkWH: -1.0, batteryStartOfCharge: 0.50)
+        let sut = BatteryCapacityCalculator(capacitykW: 8000, minimumSOC: 0.2)
+        let result = sut.batteryRemaining(batteryChargePowerkWH: -1.0, batteryStateOfCharge: 0.50)
 
         XCTAssertEqual(result, "Empty in 2 hours")
     }
-
 }
