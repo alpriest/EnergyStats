73d2bd108691d2fcb2695499a7bab40ef680af68
Al Priest <alpriest@gmail.com>
2023-10-27 09:18:51 +0100

    Support more timezone hacking when contains multiple + symbols

---
+++ Energy Stats Core Tests/FoxEssCloudParseStrategyTests.swift
@@ -0,0 +1,25 @@
+//
+//  FoxEssCloudParseStrategyTests.swift
+//  Energy Stats Core Tests
+//
+//  Created by Alistair Priest on 26/10/2023.
+//
+
+import XCTest
+@testable import Energy_Stats_Core
+
+final class FoxEssCloudParseStrategyTests: XCTestCase {
+    func test_ParsesDatesWithDoubleTimezoneEncoded() throws {
+        let date = "2023-10-26 03:10:49 +03+0300"
+        let result = try FoxEssCloudParseStrategy().parse(date)
+
+        XCTAssertEqual(result.iso8601(), "2023-10-26T00:10:49Z")
+    }
+
+    func test_ParsesDateWithGMTtimezone() throws {
+        let date = "2023-03-14 06:47:12 GMT+0000"
+        let result = try FoxEssCloudParseStrategy().parse(date)
+
+        XCTAssertEqual(result.iso8601(), "2023-03-14T06:47:12Z")
+    }
+}
--- Energy Stats Core/Network/NetworkModels/Raw.swift
+++ Energy Stats Core/Network/NetworkModels/Raw.swift
@@ -70,7 +70,42 @@ struct FoxEssCloudParseStrategy: ParseStrategy {
             return result
         }
 
+        if let result = fallbackStrategy(value) {
+            return result
+        }
+
         throw ParseFailure(value: value)
+    }
+
+    private func fallbackStrategy(_ value: String) -> Date? {
+        let regex = try! NSRegularExpression(pattern: "(\\+\\d{2})\\+\\d{4}")
+
+        if let match = regex.firstMatch(in: value, options: [], range: NSRange(location: 0, length: value.utf16.count)) {
+
+            // Extract the first time zone part
+            if let range = Range(match.range(at: 1), in: value) {
+                let timeZonePart = value[range]
+
+                // Create the new string by replacing the +XX+XXXX with +XXXX
+                let newString = value.replacingOccurrences(of: regex.pattern, with: "\(timeZonePart)00", options: .regularExpression)
+
+                let formatter = DateFormatter()
+                formatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+                formatter.locale = Locale(identifier: "en_US_POSIX")
+
+                if let result = formatter.date(from: newString) {
+                    return result
+                }
+            }
+        }
+
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+
+        let trimmed = String(value.prefix(19))
+
+        return formatter.date(from: trimmed)
     }
 }
 
