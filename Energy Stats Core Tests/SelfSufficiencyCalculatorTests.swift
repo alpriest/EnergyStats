12660da654ce073d0d6ea0169ca8e9a030889ec9
Al Priest <alpriest@gmail.com>
2023-11-15 10:36:31 +0000

    Fix unit tests

--- Energy Stats Core Tests/SelfSufficiencyCalculatorTests.swift
+++ Energy Stats Core Tests/SelfSufficiencyCalculatorTests.swift
@@ -14,42 +14,42 @@ final class AbsoluteSelfSufficiencyCalculatorTests: XCTestCase {
 
         XCTAssertEqual(result, 0.97)
         XCTAssertEqual(breakdown.formula, "1 - (min(loads, max(grid, 0.0)) / loads)")
-        XCTAssertEqual(breakdown.calculation, "1 - (min(10000.0, max(300.0, 0.0)) / 10000.0")
+        XCTAssertEqual(breakdown.calculation(1), "1 - (min(10000.0, max(300.0, 0.0) / 10000.0")
     }
 
     func test_Calculates_When_No_Grid_Usage() {
         let (result, breakdown) = AbsoluteSelfSufficiencyCalculator.calculate(loads: 10000, grid: 0)
 
         XCTAssertEqual(result, 1)
-        XCTAssertEqual(breakdown.calculation, "1 - (min(10000.0, max(0.0, 0.0)) / 10000.0")
+        XCTAssertEqual(breakdown.calculation(1), "1 - (min(10000.0, max(0.0, 0.0) / 10000.0")
     }
 
     func test_Calculates_When_Completely_Grid_Usage() {
         let (result, breakdown) = AbsoluteSelfSufficiencyCalculator.calculate(loads: 10000, grid: 10000)
 
         XCTAssertEqual(result, 0)
-        XCTAssertEqual(breakdown.calculation, "1 - (min(10000.0, max(10000.0, 0.0)) / 10000.0")
+        XCTAssertEqual(breakdown.calculation(1), "1 - (min(10000.0, max(10000.0, 0.0) / 10000.0")
     }
 
     func test_Calculates_When_Grid_Usage_Over_Load() {
         let (result, breakdown) = AbsoluteSelfSufficiencyCalculator.calculate(loads: 10000, grid: 11000)
 
         XCTAssertEqual(result, 0)
-        XCTAssertEqual(breakdown.calculation, "1 - (min(10000.0, max(11000.0, 0.0)) / 10000.0")
+        XCTAssertEqual(breakdown.calculation(1), "1 - (min(10000.0, max(11000.0, 0.0) / 10000.0")
     }
 
     func test_Calculates_When_Grid_Negative() {
         let (result, breakdown) = AbsoluteSelfSufficiencyCalculator.calculate(loads: 10000, grid: -50)
 
         XCTAssertEqual(result, 1)
-        XCTAssertEqual(breakdown.calculation, "1 - (min(10000.0, max(-50.0, 0.0)) / 10000.0")
+        XCTAssertEqual(breakdown.calculation(1), "1 - (min(10000.0, max(-50.0, 0.0) / 10000.0")
     }
 
     func test_Calculates_When_No_Loads() {
         let (result, breakdown) = AbsoluteSelfSufficiencyCalculator.calculate(loads: 0, grid: -50)
 
         XCTAssertEqual(result, 0)
-        XCTAssertEqual(breakdown.calculation, "")
+        XCTAssertEqual(breakdown.calculation(1), "")
     }
 }
 
@@ -65,7 +65,7 @@ final class NetSelfSufficiencyCalculatorTests: XCTestCase {
         Else if netGeneration + homeConsumption < 0 then result = 0
         Else if netGeneration + homeConsumption > 0 then result = (netGeneration + homeConsumption) / homeConsumption
         """)
-        XCTAssertEqual(breakdown.calculation, """
+        XCTAssertEqual(breakdown.calculation(1), """
         netGeneration = 10000.0 - 300.0 + 1000.0 - 500.0
 
         If 10200.0 > 0 then result = 1
@@ -78,7 +78,7 @@ final class NetSelfSufficiencyCalculatorTests: XCTestCase {
         let (result, breakdown) = NetSelfSufficiencyCalculator.calculate(grid: 300, feedIn: 0, loads: 10000, batteryCharge: 0, batteryDischarge: 0)
 
         XCTAssertEqual(result, 0.97)
-        XCTAssertEqual(breakdown.calculation, """
+        XCTAssertEqual(breakdown.calculation(1), """
         netGeneration = 0.0 - 300.0 + 0.0 - 0.0
 
         If -300.0 > 0 then result = 1
@@ -91,7 +91,7 @@ final class NetSelfSufficiencyCalculatorTests: XCTestCase {
         let (result, breakdown) = NetSelfSufficiencyCalculator.calculate(grid: 4000, feedIn: 0, loads: 8000, batteryCharge: 0, batteryDischarge: 0)
 
         XCTAssertEqual(result, 0.5)
-        XCTAssertEqual(breakdown.calculation, """
+        XCTAssertEqual(breakdown.calculation(1), """
         netGeneration = 0.0 - 4000.0 + 0.0 - 0.0
 
         If -4000.0 > 0 then result = 1
@@ -104,7 +104,7 @@ final class NetSelfSufficiencyCalculatorTests: XCTestCase {
         let (result, breakdown) = NetSelfSufficiencyCalculator.calculate(grid: 8000, feedIn: 0, loads: 8000, batteryCharge: 0, batteryDischarge: 0)
 
         XCTAssertEqual(result, 0.0)
-        XCTAssertEqual(breakdown.calculation, """
+        XCTAssertEqual(breakdown.calculation(1), """
         netGeneration = 0.0 - 8000.0 + 0.0 - 0.0
 
         If -8000.0 > 0 then result = 1
@@ -117,7 +117,7 @@ final class NetSelfSufficiencyCalculatorTests: XCTestCase {
         let (result, breakdown) = NetSelfSufficiencyCalculator.calculate(grid: 9000, feedIn: 0, loads: 8000, batteryCharge: 0, batteryDischarge: 0)
 
         XCTAssertEqual(result, 0.0)
-        XCTAssertEqual(breakdown.calculation, """
+        XCTAssertEqual(breakdown.calculation(1), """
         netGeneration = 0.0 - 9000.0 + 0.0 - 0.0
 
         If -9000.0 > 0 then result = 1
