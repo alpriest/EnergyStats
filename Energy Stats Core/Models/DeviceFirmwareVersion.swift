09acc4a1e7f33e1a67e3148d3fc8ce2a86ca0643
Al Priest <alpriest@gmail.com>
2024-01-10 21:00:41 +0000

    Remove version check

--- Energy Stats Core/Models/DeviceFirmwareVersion.swift
+++ Energy Stats Core/Models/DeviceFirmwareVersion.swift
@@ -18,27 +18,3 @@ public struct DeviceFirmwareVersion: Codable, Equatable, Hashable {
         self.manager = manager
     }
 }
-
-public extension Optional where Wrapped == DeviceFirmwareVersion {
-    func hasManager(greaterThan other: String) -> Bool {
-        guard let self else { return false }
-
-        let components1 = self.manager.split(separator: ".").compactMap { Int($0) }
-        let components2 = other.split(separator: ".").compactMap { Int($0) }
-
-        let maxLength = max(components1.count, components2.count)
-
-        for i in 0 ..< maxLength {
-            let v1 = components1[safe: i] ?? 0
-            let v2 = components2[safe: i] ?? 0
-
-            if v1 > v2 {
-                return true
-            } else if v1 < v2 {
-                return false
-            }
-        }
-
-        return true
-    }
-}
