94977f696990b10b710c09e3db6e3b2c7b31b7b1
Al Priest <alpriest@gmail.com>
2024-05-01 13:22:46 +0100

    Adds support for a circular complication

--- Energy Stats Watch Widgets/BatteryStatusWidget.swift
+++ Energy Stats Watch Widgets/BatteryStatusWidget.swift
@@ -12,13 +12,24 @@ import WidgetKit
 struct BatteryStatusWidget: Widget {
     let kind: String = "BatteryCornerWidget"
     @Environment(\.widgetFamily) var family
+    let circularGradient = Gradient(colors: [.red, .orange, .yellow, .green])
 
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider(deviceSN: KeychainStore().getSelectedDeviceSN())) { entry in
             Group {
                 if let soc = entry.soc {
-                    Text(soc, format: .percent)
-                        .widgetLabel("SOC")
+                    switch family {
+                    case .accessoryCircular:
+                        Gauge(value: Double(soc), in: 0 ... 100) {
+                            Image(systemName: "minus.plus.batteryblock.fill")
+                                .font(.system(size: 14))
+                        } currentValueLabel: {
+                            Text(soc, format: .percent)
+                        }.gaugeStyle(CircularGaugeStyle(tint: circularGradient))
+                    default:
+                        Text(soc, format: .percent)
+                            .widgetLabel("SOC")
+                    }
                 } else {
                     if let errorMessage = entry.errorMessage {
                         Text("")
@@ -37,11 +48,12 @@ struct BatteryStatusWidget: Widget {
         .description("Shows the status of your home battery")
         .supportedFamilies([.accessoryInline,
                             .accessoryCorner,
+                            .accessoryCircular,
                             .accessoryRectangular])
     }
 }
 
-#Preview(as: .accessoryCorner) {
+#Preview(as: .accessoryCircular) {
     BatteryStatusWidget()
 } timeline: {
     SimpleEntry(date: .now, soc: 25, chargeStatusDescription: "S", state: .loaded, errorMessage: nil)
